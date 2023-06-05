1ecd93f221bb2ca56ca07eaf4fa5b01206204636
Kevin Chang <kevinchang@qnap.com>
2023-06-05 17:37:45 +0800

    add more deadcode test case

--- deadcode.js
+++ deadcode.js
@@ -27,6 +27,7 @@ function testDeadCode(q) {
     if (!Array.isArray(q?.a)) {
         return '1';
     }
+    // Did't detect dead code in coverity
     if (q?.a === undefined) return 'imposable';
 
     const str = q?.a[0];
@@ -34,6 +35,25 @@ function testDeadCode(q) {
     if (typeof str !== 'string') return '2';
 
     return '3';
+}
+
+function testDeadCode2(q) {
+    if (!Number.isNaN(q)) {
+        return '1';
+    }
+
+    if (q == null) return 'imposable';
+
+    return '2'
+}
+
+function testDeadCode3(q) {
+    if (!(typeof q === 'string')) {
+        return '1';
+    }
+    if (q == null) return 'imposable';
+
+    return '2'
 }
 
 function test7a(p1) {
