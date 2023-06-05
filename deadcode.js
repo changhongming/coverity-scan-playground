aa599e5fbb4ecc0831aa35d57a80aa3851cf6997
Kevin Chang <kevinchang@qnap.com>
2023-06-05 17:24:16 +0800

    add deadcode test

--- deadcode.js
+++ deadcode.js
@@ -8,30 +8,39 @@ const instance = {
         if (typeof str !== 'string') return '2';
 
         return '3';
+    },
+    testDeadCode(q) {
+        if (!Array.isArray(q?.a)) {
+            return '1';
+        }
+        if (q?.a === undefined) return 'imposable';
+    
+        const str = q?.a[0];
+    
+        if (typeof str !== 'string') return '2';
+    
+        return '3';
+    },
+}
+
+function testDeadCode(q) {
+    if (!Array.isArray(q?.a)) {
+        return '1';
     }
+    if (q?.a === undefined) return 'imposable';
+
+    const str = q?.a[0];
+
+    if (typeof str !== 'string') return '2';
+
+    return '3';
 }
 
 function test7a(p1) {
     if(p1 == null) {
         return;
     }
-    // In JavaScript null == undefined.
-    if(p1 == undefined) {
-        // Defect: dead code
-        return;
-    }
-    safe_process(p1);
-}
 
-function test7b(p1) {
-    if(p1 === null) {
-        return;
-    }
-    // But null !== undefined.
-    if(p1 === undefined) {
-        // No defect
-        return;
-    }
     safe_process(p1);
 }
 
@@ -41,9 +50,5 @@ function adapted_angular_code_example(parentElement) {
   var result = state && (!!state.disabled || state.totalActive > 0);
   if(isRoot || result) {
     return result;
-  }
-  if(isRoot) {
-    // Defect: dead code
-    return true;
   }
 }
