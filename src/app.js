cbc5a07a0f8178f8a47b97392160f93bf33ec68c
Kevin Chang <kevinchang@qnap.com>
2023-08-23 17:35:28 +0800

    ts testing

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+function doSomething(data : DataType) {
+    const age1 = data.child[0].age
+    const age2 = data.child[1].age
+
+    return age1 + age2;
+}
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+interface DataType {
+    name: string;
+    id: number;
+    value: string | number;
+    child: Array<ChildType>;
+}
+
+interface ChildType {
+    name: string;
+    age: number;
+}
+
+function doSomething(data : DataType) {
+    const age1 = data.child[0].age
+    const age2 = data.child[1].age
+
+    return age1 + age2;
+}
