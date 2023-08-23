fed18e027e2b6526a5d3d07683e411def7d0a663
Kevin Chang <kevinchang@qnap.com>
2023-08-23 17:39:43 +0800

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 
-function doSomething(data : DataType) {
+function doSomething(data) {
     const age1 = data.child[0].age
     const age2 = data.child[1].age
+
+    return age1 + age2;
+}
+
+function doSomething2(data) {
+    const age1 = data.child?.[0]?.age
+    const age2 = data.child?.[1]?.age
 
     return age1 + age2;
 }
--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,10 @@ function doSomething(data : DataType) {
 
     return age1 + age2;
 }
+
+function doSomething2(data : DataType) {
+    const age1 = data.child?.[0]?.age
+    const age2 = data.child?.[1]?.age
+
+    return age1 + age2;
+}
